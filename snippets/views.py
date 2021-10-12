457eba7de6e6ca8977fd03f7294641d886e4b549
quanshuli <quanshuli.dev@gmail.com>
2021-10-12 10:13:44 +1000

    Update views.py

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework.serializers import Serializer
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+@csrf_exempt
+def snippet_list(request):
+    def snippet_list(request):
+        if request.method == 'GET':
+            snippets = Snippet.objects.all()
+            serializer = SnippetSerializer(snippets, many=True)
+            
