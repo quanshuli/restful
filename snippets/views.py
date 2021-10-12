7e7d73d91d4088daa16ab50e352aa88c1b8d50e7
quanshuli <quanshuli.dev@gmail.com>
2021-10-12 11:57:11 +1000

    Update views.py

--- snippets/views.py
+++ snippets/views.py
@@ -1,8 +1,8 @@
+import re
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import serializers
 from rest_framework.parsers import JSONParser
-from rest_framework.serializers import Serializer
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
@@ -19,8 +19,35 @@ def snippet_list(request):
             serializer = SnippetSerializer(data=data)
             if serializer.is_valid():
                 serializer.save()
+                # 201: created
                 return JsonResponse(serializer.data, status=201)
+            # 400: bad request
             return JsonResponse(serializer.errors, status=400)
 
-            
+@csrf_exempt
+def snippet_detail(request, pk):
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
+
+
+
+     
 
