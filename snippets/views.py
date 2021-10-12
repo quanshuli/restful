00694314482b3896a61ce8e24c47cb6439069de5
quanshuli <quanshuli.dev@gmail.com>
2021-10-12 10:16:23 +1000

    Update views.py

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import serializers
 from rest_framework.parsers import JSONParser
 from rest_framework.serializers import Serializer
 from snippets.models import Snippet
@@ -11,4 +12,15 @@ def snippet_list(request):
         if request.method == 'GET':
             snippets = Snippet.objects.all()
             serializer = SnippetSerializer(snippets, many=True)
+            return JsonResponse(serializer.data, safe=False)
+
+        elif request.method == 'POST':
+            data = JSONParser().parse(request)
+            serializer = SnippetSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.errors, status=400)
+
             
+
